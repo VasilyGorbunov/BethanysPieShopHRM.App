0e0397b1594a844449b479fe818250d424841868
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-02-18 22:42:30 +0300

    06 -done

--- BethanysPieShopHRM.App/Pages/EmployeeDetail.razor.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeDetail.razor.cs
@@ -1,7 +1,9 @@
 ﻿using BethanysPieShopHRM.App.Models;
 using BethanysPieShopHRM.App.Services;
 using BethanysPieShopHRM.Shared.Domain;
+using BethanysPieShopHRM.Shared.Model;
 using Microsoft.AspNetCore.Components;
+using System.Net.Http.Headers;
 
 namespace BethanysPieShopHRM.App.Pages;
 
@@ -15,8 +17,24 @@ public partial class EmployeeDetail
 
     public Employee Employee { get; set; } = new Employee();
 
+    public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
     protected async override Task OnInitializedAsync()
     {
         Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+
+        if (Employee.Longitude.HasValue && Employee.Latitude.HasValue)
+        {
+            MapMarkers = new List<Marker> 
+            { 
+                new Marker
+                {
+                    Description = $"{Employee.FirstName} {Employee.LastName}",
+                    ShowPopup = false,
+                    X = Employee.Longitude.Value,
+                    Y = Employee.Latitude.Value,
+                }
+            };
+        }
     }
 }
